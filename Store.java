6925648f3446ca0027c51b90a0d5191764189b75
romkhonglun <cong.nh225476@sis.hust.edu.vn>
2024-04-27 22:29:33 +0700

    Store test

---
+++ Store.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Store {
+    public static final int MAX_NUMBERS_STORE = 1000;
+    DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_NUMBERS_STORE];
+    DigitalVideoDisc[] tempInStore = new DigitalVideoDisc[MAX_NUMBERS_STORE];
+
+    private int counting = 0;
+
+    public void addDVD(DigitalVideoDisc disc) {
+        if (counting < MAX_NUMBERS_STORE) {
+            itemsInStore[counting] = disc;
+            counting++;
+            System.out.println("The disc has been added.");
+        } else {
+            System.out.println("No more space in store.");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        int cnt = 0;
+        for (int i = 0; i < counting; i++) {
+            if (itemsInStore[i].getTitle() != disc.getTitle()) {
+                tempInStore[cnt] = itemsInStore[i];
+                cnt++;
+            }
+        }
+        if (cnt == counting) {
+            System.out.println("The disc " + '"' + disc.getTitle() + '"' + " is not in the cart.");
+        } else {
+            itemsInStore = Arrays.copyOf(tempInStore, cnt);
+            counting = cnt;
+            System.out.println("The disc " + '"' + disc.getTitle() + '"' + " has been removed.");
+        }
+    }
+
+}
---
+++ StoreTest.java
@@ -0,0 +1,12 @@
+package hust.soict.dsai.aims.store;
+
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+
+public class StoreTest {
+    public static void main(String[] args){
+        Store store = new Store();
+        DigitalVideoDisc dvd_new = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        store.addMedia(dvd_new);
+
+    }
+}
