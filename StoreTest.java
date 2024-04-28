1844530b68c890a6f3c6f4ab0333cf01a9a8b80f
romkhonglun <154040668+romkhonglun@users.noreply.github.com>
2024-04-28 11:14:14 +0700

    Update StoreTest.java

--- StoreTest.java
+++ StoreTest.java
@@ -1,6 +1,6 @@
 package hust.soict.dsai.aims.store;
 
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.DigitalVideoDisc;
 
 public class StoreTest {
     public static void main(String[] args){
