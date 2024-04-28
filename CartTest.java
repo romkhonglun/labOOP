763c96e50a4304b5350816f35113024115c2aafc
romkhonglun <154040668+romkhonglun@users.noreply.github.com>
2024-04-28 10:40:17 +0700

    Update CartTest.java

--- CartTest.java
+++ CartTest.java
@@ -5,7 +5,7 @@ public class CartTest {
         cart.addDigitalVideoDisc(dvd1);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 97, 24.95f);
         cart.addDigitalVideoDisc(dvd2);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
         cart.addDigitalVideoDisc(dvd3);
 
         cart.print()
