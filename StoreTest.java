26b533ab565ed83ddaa4721740e8eaf5eef87795
romkhonglun <154040668+romkhonglun@users.noreply.github.com>
2024-04-28 11:15:32 +0700

    Update StoreTest.java

--- StoreTest.java
+++ StoreTest.java
@@ -6,7 +6,7 @@ public class StoreTest {
     public static void main(String[] args){
         Store store = new Store();
         DigitalVideoDisc dvd_new = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        store.addMedia(dvd_new);
+        store.addDVD(dvd_new);
 
     }
 }
